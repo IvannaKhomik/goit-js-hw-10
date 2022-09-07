1ab1e844a445cfbdfd00a2a9e06aec045cf91808
IvannaKhomik <ivanna.khomik@gmail.com>
2022-09-07 05:25:32 +0300

    add changes for searchcountry js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,81 @@
 import './css/styles.css';
+// Import
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+import { fetchCountry } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector('input#search-box');
+const list = document.querySelector('.country-list');
+const card = document.querySelector('.country-info');
+
+input.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
+
+function onInputChange(e) {
+  const value = e.target.value.trim();
+  fetchCountry(value).then(data => {
+    return renderCountry(data);
+  });
+}
+
+const renderList = data => {
+  list.innerHTML = '';
+  card.innerHTML = '';
+
+  const items = data
+    .map(
+      ({
+        flags: { svg },
+        name: { official },
+      }) => `<li class="country-list-item">
+    <img class="country-flag" src="${svg}" alt="${official}" width="26px">
+    <div class="country-name">${official}</div>
+  </li>`
+    )
+    .join('');
+  list.insertAdjacentHTML('afterbegin', items);
+};
+
+const renderCard = data => {
+  list.innerHTML = '';
+  card.innerHTML = '';
+
+  const item = data
+    .map(
+      ({
+        flags: { svg },
+        name: { official },
+        capital,
+        population,
+        languages,
+      }) => `<div class="country-item">
+    <img class="country-flag" src="${svg}" alt="${official}" width="26px">
+    <div class="country-name">${official}</div>
+  </div>
+  <ul>
+  <li class="country-desc"><span>Capital: </span>${capital}</li>
+  <li class="country-desc"><span>Population: </span>${population}</li>
+  <li class="country-desc"><span>Languages: </span>${Object.values(
+    languages
+  ).join(', ')}</li>
+  </ul>`
+    )
+    .join('');
+
+  card.insertAdjacentHTML('afterbegin', item);
+};
+
+function renderCountry(data) {
+  if (data.length > 1 && data.length <= 10) {
+    renderList(data);
+  } else if (data.length === 1) {
+    renderCard(data);
+  } else if (data.length > 10) {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+  } else {
+    list.innerHTML = '';
+    card.innerHTML = '';
+    Notify.failure('Oops, there is no country with that name');
+  }
+}
